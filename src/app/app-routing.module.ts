60fe6d1ce99fd5ac82046ecb2b358bc391a33bbc
aminabishaikh <139878284+aminabishaikh@users.noreply.github.com>
2023-11-02 11:22:13 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './application/pages/login/login.component';
+import { ForgotPasswordComponent } from './application/pages/forgot-password/forgot-password.component';
+import { VerificationComponent } from './application/pages/verification/verification.component';
+import { NewPasswordComponent } from './application/pages/new-password/new-password.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route redirects to '/home'
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verification', component: VerificationComponent },
+  { path: 'new-password', component: NewPasswordComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './application/pages/login/login.component';
 import { SplashComponent } from './application/pages/splash/splash.component';
+import { ForgotPasswordComponent } from './application/pages/forgot-password/forgot-password.component';
+import { VerificationComponent } from './application/pages/verification/verification.component';
+import { NewPasswordComponent } from './application/pages/new-password/new-password.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, LoginComponent, SplashComponent],
+  declarations: [AppComponent, HomepageComponent, LoginComponent, SplashComponent, ForgotPasswordComponent, VerificationComponent, NewPasswordComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
