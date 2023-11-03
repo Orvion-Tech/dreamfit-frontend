1554e51600fd3684e4473e2ddaf7f0778cea19b5
ravispicules <ravi.patel@archinfotech.co>
2023-11-03 16:01:49 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -18,7 +18,7 @@ import { RegistrationComponent } from './application/pages/registration/registra
     ForgotPasswordComponent,
     RegistrationComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/application/pages/login/login.component.ts
+++ src/app/application/pages/login/login.component.ts
@@ -1,8 +1,82 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+  countryCode = false;
+  phoneNumber = false;
+  password = false;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      country_code: ['', Validators.required],
+      phone_number: ['', [Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
+  // ngOnInit(): void {
+  //   const idToken = localStorage.getItem('id_token');
+  //   const userId = localStorage.getItem('user_id');
+
+  //   if (idToken && userId) {
+  //     window.location.href = '/account'; // Allow access to the route
+  //   }
+  // }
+  async onSubmit() {
+    this.countryCode = true;
+    this.password = true;
+    this.phoneNumber = true;
+
+    if (this.loginForm.valid) {
+      const credentials = {
+        phone_number: this.loginForm.value.country_code + this.loginForm.value.phone_number,
+        password: this.loginForm.value.password,
+      };
+      try {
+        const response = await fetch('http://192.168.1.28/api/login/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(credentials),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          const user_id = data.user_id;
+          localStorage.setItem('id_token', data.token.access);
+          localStorage.setItem('user_id', user_id);
+          // const now: any = new Date().getTime();
+          // localStorage.setItem('token_timestamp', now);
+          // window.location.replace('/account');
+        } else {
+          const data = await response.json();
+          alert(data.message);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    } else {
+      const usernameCont = this.loginForm.get('phone_number');
+      const countryCodeCont = this.loginForm.get('country_code');
+      const passwordCont = this.loginForm.get('password');
+      if (usernameCont) {
+        this.phoneNumber = usernameCont.invalid;
+      }
+      if (countryCodeCont) {
+        this.countryCode = countryCodeCont.invalid;
+      }
+      if (passwordCont) {
+        this.password = passwordCont.invalid;
+      }
+    }
+  }
+  resetError() {
+    this.phoneNumber = false;
+    this.countryCode = false;
+    this.password = false;
+  }
+}
