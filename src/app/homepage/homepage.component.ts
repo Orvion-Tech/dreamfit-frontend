196e5fef4a67a744953e3bd3d6ffc9789f98b698
aminabishaikh <139878284+aminabishaikh@users.noreply.github.com>
2023-11-23 12:13:23 +0530

    update

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
   showMenu = false;
+  constructor(private renderer: Renderer2) {}
+
+  showMenuFn() {
+    this.showMenu = !this.showMenu;
+
+    if (this.showMenu) {
+      this.renderer.addClass(document.body, 'noscroll');
+    } else {
+      this.renderer.removeClass(document.body, 'noscroll');
+    }
+  }
 }
