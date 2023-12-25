2c391082d55085d0b4871de849d6acb221d986f5
ravispicules <ravi.patel@archinfotech.co>
2023-12-25 17:05:25 +0530

    UPDATE

--- src/app/application/components/app-header/app-header.component.ts
+++ src/app/application/components/app-header/app-header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss'],
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent {
+  showMenu = false;
+  constructor(private renderer: Renderer2) {}
+  logout() {
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('token_timestamp');
+    window.location.replace('/');
+  }
+  showMenuFn() {
+    this.showMenu = !this.showMenu;
+
+    if (this.showMenu) {
+      this.renderer.addClass(document.body, 'noscroll');
+    } else {
+      this.renderer.removeClass(document.body, 'noscroll');
+    }
+  }
+}
